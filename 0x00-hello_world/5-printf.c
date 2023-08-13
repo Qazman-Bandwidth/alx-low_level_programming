bf2c60e233b8b8e4bb4570a19b5a8e3379e66e1b
Qazman-Bandwidth <kazeem.lukman.ola@gmail.com>
2023-08-13 11:48:49 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,11 +2,15 @@
 
 /**
  * main - Hello, printf
- * Return: Always (0) success
+ * 
+ * Return: Always (0) Success
  */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art, ");
+	printf("with proper grammar, but the outcome is a piece of art,");
+
 	return(0);
+
+
 }
