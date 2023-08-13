c0242261bb96ff4322826ec9a8f9be2f794f754b
Qazman-Bandwidth <kazeem.lukman.ola@gmail.com>
2023-08-13 12:31:41 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - 
+ * Return: Always (0) Success
+ */
+
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+
+	printf("Size of a char: %lu bytes(s)");
+	printf("Size of a int: %lu bytes(s)");
+	printf("Size of a long int: %lu bytes(s)");
+	printf("Size of a long long int: %lu bytes(s)");
+	printf("Size of a float: %lu bytes(s)");
+
+	return (0);
+
+
+}
