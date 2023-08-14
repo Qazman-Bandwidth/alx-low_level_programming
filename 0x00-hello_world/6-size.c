5764c7b81ecbeae23e1db97db8ff6dd79b6d6f1e
Qazman-Bandwidth <kazeem.lukman.ola@gmail.com>
2023-08-13 23:28:07 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,17 @@
  */
 int main(void)
 {
-printf("Size of a char: %lu bytes(s)\n", (unsigned long)sizeof(char));
-printf("Size of an int: %lu bytes(s)\n", (unsigned long)sizeof(int));
-printf("Size of a long int: %lu bytes(s)\n", (unsigned long)sizeof(long int));
-printf("Size of a long long int: %lu bytes(s)\n", (unsigned long)sizeof(long long int));
-printf("Size of a float: %lu bytes(s)\n", (unsigned long)sizeof(float));
-return (0);
+char a;
+int b;
+long int c;
+long long int d;
+float e;
+	
+
+printf("Size of a char: %lu byte(s)\n", sizeof(a));
+printf("Size of an int: %lu byte(s)\n", sizeof(b));
+printf("Size of a long int: %lu byte(s)\n", sizeof(c));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(d));
+printf("Size of a float: %lu byte(s)\n", sizeof(e))
+	return (0);
 }
