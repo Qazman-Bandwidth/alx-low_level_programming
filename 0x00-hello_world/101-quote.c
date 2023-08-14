908a089d5bd3219cdcb0ff8eb2a90265d006bfeb
Qazman-Bandwidth <kazeem.lukman.ola@gmail.com>
2023-08-13 22:51:42 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - A program to print in UNIX
+ * Return: 1 (success)
+ */
+
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+
+
+}
