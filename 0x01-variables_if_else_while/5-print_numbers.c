8fa363d5d7a632f7e08f6b273da83fed9d8b293e
Qazman-Bandwidth <kazeem.lukman.ola@gmail.com>
2023-08-14 20:52:49 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints all single digit numbers to base 0
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	for (int i = 0; i < 10; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
