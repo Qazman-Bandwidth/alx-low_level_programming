b02c56dcc5811f0d0aa58be4e51d7d37277e8f41
Qazman-Bandwidth <kazeem.lukman.ola@gmail.com>
2023-08-14 21:55:21 -0700

    7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -8,9 +8,9 @@
 
 int main(void)
 {
-	char i;
-	for (i = 'z'; i >= 'a'; i--)
-		putchar(i);
-	putchar('\n');
-	return (0);
+char i;
+for (i = 'z'; i >= 'a'; i--)
+putchar(i);
+putchar('\n');
+return (0);
 }
