2bc068717d3d0fd144ded28a811d512afedcf8ac
Qazman-Bandwidth <kazeem.lukman.ola@gmail.com>
2023-08-14 22:54:12 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,4 +6,25 @@
  * Return: 0 always
  */
 
-
+int main(void)
+{
+	int i, j;
+	for (i = 48; i <= 56; i++)
+	{
+		for (j = 49; j <= 57; j++)
+		{
+			if (j > i)
+			{
+				putchar(i);
+				putchar(j);
+				if (i != 56 || j != 57)
+				{
+					putchar(',');
+					putchar('');
+				}
+			}
+		}
+	}
+	putchar('n');
+	return (0);
+}
