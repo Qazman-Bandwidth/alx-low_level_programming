36f71f341c00db6dc8627e18e7b4df7cd5d6fffa
Qazman-Bandwidth <kazeem.lukman.ola@gmail.com>
2023-08-14 21:10:22 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints the lower case alphabet in reverse
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	char i;
+	for (i = 'z'; i >= 'a'; i--)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
