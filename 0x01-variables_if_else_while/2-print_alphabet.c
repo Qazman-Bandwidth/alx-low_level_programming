6164dac090486651ba0e930606d249b95621f115
Qazman-Bandwidth <kazeem.lukman.ola@gmail.com>
2023-08-14 20:16:10 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - A program that print alphabet in lower case
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	for (i='a'; i<='z'; i++)
+		putchar(i);
+	putchar('\n');
+
