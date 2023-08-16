01c643f9f84846348514348e8abec5c23638087a
Qazman-Bandwidth <kazeem.lukman.ola@gmail.com>
2023-08-16 15:13:19 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * add - a function that adds two integers and returns the result
+ * @num1: is the first integer
+ * @num2: is the second integer
+ * Return: Integer value
+ */
+
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
