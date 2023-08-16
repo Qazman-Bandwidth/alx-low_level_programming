188ea421f6cd575577fb5bc6fa6c27351231c578
Qazman-Bandwidth <kazeem.lukman.ola@gmail.com>
2023-08-16 06:28:34 -0700

    6- abs

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,17 +1,18 @@
 #include "main.h"
 
 /**
- * main -  A function that computes the absolute value of an integer
+ * _abs -  A function that computes the absolute value of an integer
+ * @i: The parameter to return
  * Return: absolute value of a number
  */
 
 int _abs(int i)
 {
-	int abs_val;
-	if (i < 0)
-	{
-		abs_val = i * -1;
-		return (abs_val);
-	}
-	return (i);
+int abs_val;
+if (i < 0)
+{
+abs_val = i * -1;
+return (abs_val);
+}
+return (i);
 }
