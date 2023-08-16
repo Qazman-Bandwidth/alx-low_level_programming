8f1fbbaf52a3c9952c41a8548c2de9a4dbdfbc6e
Qazman-Bandwidth <kazeem.lukman.ola@gmail.com>
2023-08-15 23:12:03 -0700

    2-alphabet_x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,16 +8,16 @@
 
 void print_alphabet_x10(void)
 {
-	char i;
-	int j = 0;
+char i;
+int j = 0;
 
-	while (j <= 9)
-	{
-	for (i = 'a'; i <= 'z'; i++)
-	{
-	_putchar(i);
-	}
-	_putchar('\n');
-	i++;
-	}
+while (j <= 9)
+{
+for (i = 'a'; i <= 'z'; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+j++;
+}
 }
