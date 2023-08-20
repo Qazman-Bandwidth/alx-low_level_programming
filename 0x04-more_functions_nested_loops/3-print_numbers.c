8ce260c913c4b214b32a9d1634fc6c422f36e621
Qazman-Bandwidth <kazeem.lukman.ola@gmail.com>
2023-08-20 07:56:02 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_numbers: function to prints the numbers from 0 to 9
+ * print_numbers - function to prints the numbers from 0 to 9
  * @c: The character to be checked
  * Return: 0 or 1
  */
@@ -9,9 +9,9 @@
 void print_numbers(void)
 {
 	char c;
-	for (c = '0'; c <= '9'; c++)
-	{
-		_putchar(c);
-	}
-	_putchar('\n');
+for (c = '0'; c <= '9'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
 }
