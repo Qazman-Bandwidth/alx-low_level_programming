4a615dcced04d93a560339335d6fbd3ee9e30127
Qazman-Bandwidth <kazeem.lukman.ola@gmail.com>
2023-08-20 08:26:05 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,22 +2,20 @@
 
 /**
  * more_numbers - A function that prints 10 times the numbers from 0 to 14
- * 
  * Return: numbers
  */
-
 void more_numbers(void)
 {
-	int i, j;
-
-	for (i = j; i <= 10; i++)
-	{
-		for (j = 0; j <= 14; j++)
-		{
-			if (j >= 10)
-				_putchar('1');
-			_putchar (j % 10 + '0');
-		}
-		_putchar('\n');
-	}
-}	
+int i = 0;
+int j = 0;
+for (i = j; i <= 10; i++)
+{
+for (j = 0; j <= 14; j++)
+{
+if (j >= 10)
+_putchar('1');
+_putchar (j % 10 + '0');
+}
+_putchar('\n');
+}
+}
