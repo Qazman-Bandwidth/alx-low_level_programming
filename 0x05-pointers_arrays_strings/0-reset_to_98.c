25249e06fb02fd5683ef7c9338e79bc2be2d41a5
Qazman-Bandwidth <kazeem.lukman.ola@gmail.com>
2023-08-21 21:26:18 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reset_to_98 - A function that takes a pointer to an int as parameter
+ * @n: is the variable
+ * return: n
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
