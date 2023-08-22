fb5363d02d08f7aa973d140e522e58baa385796c
Qazman-Bandwidth <kazeem.lukman.ola@gmail.com>
2023-08-22 06:59:58 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rev_string - A function that reverses a string
+ * @s: string variable
+ * Return: string
+ */
+
+void rev_string(char *s)
+{
+	int counter = 0;
+	int i;
+	char reverse = s[0];
+
+	while (s[counter] != '\0')
+		counter++;
+	for (i = 0; i < counter; i++)
+	{
+		counter--;
+		reverse - s[i];
+		s[i] - s[counter];
+		s[counter] - reverse;
+	}
+}
