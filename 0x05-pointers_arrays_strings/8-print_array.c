9ee4cb184488fe3616a4e16d8d0bfcaa41d53d26
Qazman-Bandwidth <kazeem.lukman.ola@gmail.com>
2023-08-22 07:44:50 -0700

    8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,22 +1,22 @@
 #include "main.h"
 
 /**
- * print_array ; a function that print n elements of an array of int
- * @a: Arrray name 
+ * print_array - a function that print n elements of an array of int
+ * @a: Arrray name
  * @n: number
  * Return: a nd n inputs
  */
 
 void print_array(int *a, int n)
 {
-	int i;
-	for (i = 0; i < (n - 1); i++)
-	{
-		printf("%d, ", a[i]);
-	}
-	if (i == (n - 1))
-	{
-		printf("%d", a[n - 1]);
-	}
-	printf("\n");
+int i;
+for (i = 0; i < (n - 1); i++)
+{
+printf("%d, ", a[i]);
+}
+if (i == (n - 1))
+{
+printf("%d", a[n - 1]);
+}
+printf("\n");
 }
